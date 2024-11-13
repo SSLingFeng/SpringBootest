3c433d4aa28cd52e7b2688926ec1db89a5db24bc
czwMainEngine <582390473@qq.com>
2024-11-13 23:33:01 +0800

    test1

--- src/main/java/com/example/springbootservice/Controller/testSql.java
+++ src/main/java/com/example/springbootservice/Controller/testSql.java
@@ -1,24 +1,31 @@
 package com.example.springbootservice.Controller;
 
 
+import cn.hutool.extra.ssh.JschUtil;
+import cn.hutool.json.JSONUtil;
 import com.example.springbootservice.sqlTool.MysqlTool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.List;
 
 @RestController
+@Service
 public class testSql {
-
+    @Autowired
+    private MysqlTool mysqlTool;
 
     @GetMapping("/testSQL")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-
-        MysqlTool mysqlTool = new MysqlTool();
-
-//        List<Object> list=mysqlTool.
-
-        return String.format("Hello %s!", name);
+    public String testSQL(@RequestParam(value = "name", defaultValue = "World") String name) {
+        String sql = "select * from download limit 1";
+//        List<Object> list=mysqlTool.executeQueryForList(sql,null);
+        List<Object> list = Collections.singletonList(mysqlTool.executeQueryForMapList(sql));
+//        System.out.println(list);
+        String json = JSONUtil.toJsonStr(list);
+        return json;
     }
 }
